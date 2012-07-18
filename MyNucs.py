d00ca12f705f6b242bf7f42003e045e7aa26c10d
Yvan <yvan.strahm@gmail.com>
2012-07-18 15:20:41 +0200

    get_alphabet works

--- MyNucs.py
+++ MyNucs.py
@@ -1,7 +1,20 @@
 from Bio.Seq import Seq
 from Bio.Alphabet import IUPAC
 seq = raw_input("enter your sequence:  ")
-my_seq = Seq(seq, IUPAC.unambiguous_dna)
+
+my_s = Seq(seq)
+print my_s
+my_s.alphabet()
+s2 = Seq('ACGTACGTACGTACGT', IUPAC.IUPACAmbiguousDNA())
+try:
+    assert (isinstance (IUPAC.Alphabet._get_base_alphabet(s2.alphabet), IUPAC.ExtendedIUPACDNA))
+except AssertionError:
+    print "wrong type"
+
+isinstance (IUPAC.Alphabet._get_base_alphabet(s2.alphabet), IUPAC.ExtendedIUPACDNA)
+>>> False
+#my_seq = Seq(seq, IUPAC.unambiguous_dna)
+my_seq = Seq(seq, IUPAC.extended_dna)
 
 print "original\t\t", my_seq
 print "complement\t\t", my_seq.complement()
