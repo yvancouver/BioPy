9657c61f2fe84821bea2870000d2ee396324a345
Yvan <yvan.strahm@gmail.com>
2012-10-11 15:14:03 +0200

    rename bed works

--- renameBed.py
+++ renameBed.py
@@ -1,43 +1,45 @@
 import pybedtools
 import sys
 import re
+import cardio_dict
 
-def check_introns(genes_dic):
+def check_exons(exons_list,strand):
     # get the length of the dico , this in the number of intron
     # check the strand if minus/r rename exon from big to small
     # if positive/f rename from small to big
     # rename need to be on the name
     # NM_000364_cds_0_20_chr1_201328338_r
-    pass
-
+    # to 
+    # NM_004949_DSC2_6_20_r    0    -
+    number_exons = len(exons_list)
+    pattern = re.compile("(NM_\d+)_cds_(\d+)(_\d+)_chr\d+_\d+_(\w)")
+    i = 0
+    for exon in exons:
+        if exon.strand == "+":
+            result = pattern.match(exon.name)
+            print exon.chrom+"\t"+str(exon.start)+"\t"+str(exon.stop)+"\t"+result.group(1)+"_"+cardio_dict.Cardio_dict[result.group(1)]+"_exon_"+str(i+1)+"_Flanking"+result.group(3)+"_"+result.group(4)+"\t"+exon.score+"\t"+exon.strand
+            i += 1
+        else:
+            result = pattern.match(exon.name)
+            print exon.chrom+"\t"+str(exon.start)+"\t"+str(exon.stop)+"\t"+result.group(1)+"_"+cardio_dict.Cardio_dict[result.group(1)]+"_exon_"+str(number_exons-i)+"_Flanking"+result.group(3)+"_"+result.group(4)+"\t"+exon.score+"\t"+exon.strand
+            i += 1
 bed_file = pybedtools.BedTool("/Users/yvans/Home/Dropbox/travail/BED_GFF_INTERVALS/UseMe/Cardio/Ucsc_Coding_exons_Cardio_list_b37+-20.bed")
-genes = []
+exons = []
 feature = "None"
-
+strand_dict ={'f':"+", 'r':"-"}
+strand_letter = ""
+seed = re.compile("(NM_\d+)_cds_\d+_\d+_chr\d+_\d+_(\w)")
 for line in bed_file:
-    #print line
-    #print line.start
-    chrom = ""
-    start = 0.1
-    stop = 0.1
-    
-    colors = "255,0,0"
-    i = 1
-    notSee = True
-    seed = re.compile("(NM_\d+)_cds_")
     result = seed.match(line.name)
-    #print result.group(1) , line.name , feature
-    #print type(result.group(1)) , type(feature)
     if feature != result.group(1):
-        #print result.group(1)
-        for intron in genes:
-            print str(intron).strip()
-        check_introns(genes)
-        print
-        print    
-        genes = []
-        genes.append(line)
+        check_exons(exons,strand_letter)
+        exons = []
+        exons.append(line)
         feature = result.group(1)
-
+        strand_letter = result.group(2)
+        strand_sign = line.strand
     else:
-        genes.append(line)
+        if strand_letter != result.group(2):
+            sys.exit("WRONG STRAND")
+        exons.append(line)
+check_exons(exons,strand_letter)
