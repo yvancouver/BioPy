d98202e56719e875464837d7624e9b0502a21753
Yvan <yvan.strahm@gmail.com>
2012-11-07 12:00:58 +0100

    pass the file as arg

--- CountMe.py
+++ CountMe.py
@@ -4,6 +4,8 @@
 
 import cardio_dict
 from Bio import SeqIO
+import sys
+fasta_file = sys.argv[1]
 
-for seq_record in SeqIO.parse("//Users/yvans/Home/Dropbox/travail/BED_GFF_INTERVALS/UseMe/Cardio/FinalFiles/AllCardio.fasta", "fasta"):
+for seq_record in SeqIO.parse(fasta_file, "fasta"):
     print seq_record.id,"\t", len(seq_record),"\t",cardio_dict.Cardio_dict[seq_record.id[:-2]]
